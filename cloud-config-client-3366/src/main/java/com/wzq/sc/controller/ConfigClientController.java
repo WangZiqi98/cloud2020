a9c9fac8451ae5d1b64e3329115ab010327e7ad6
WangZiqi98 <seemed_wzq@163.com>
2020-03-30 14:21:16 +0800

    Bus配合RabbitMQ实现一次发布 处处转达

--- cloud-config-client-3366/src/main/java/com/wzq/sc/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/wzq/sc/controller/ConfigClientController.java
@@ -1,11 +1,14 @@
 package com.wzq.sc.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
@@ -17,6 +20,6 @@ public class ConfigClientController {
 
     @GetMapping("/getConfigInfo")
     public String getConfigInfo() {
-        return "serverPort: " + serverPort + "\t\n\n configInfo: " + configInfo;
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
     }
 }
